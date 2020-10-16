24706e08090c0a53d741f18918d655ed3075f1e0
DylanAvery <dylavery712@gmail.com>
2020-10-16 09:34:15 -0400

    Part 5 complete

---
+++ src/app/prodect-alert/prodect-alert.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-prodect-alert',
+  templateUrl: './prodect-alert.component.html',
+  styleUrls: ['./prodect-alert.component.css']
+})
+export class ProdectAlertComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
